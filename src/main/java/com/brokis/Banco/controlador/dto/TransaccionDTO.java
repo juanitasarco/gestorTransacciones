a7c2a04e02e65d3cc0f1d3a46b166a0bd119b1bc
SebassPP <juanpapa@unisabana.edu.co>
2023-04-05 21:38:50 -0500

    Implementa @AllArgsConstructor y @NoArgsConstructor a los DTO

--- src/main/java/com/brokis/Banco/controlador/dto/TransaccionDTO.java
+++ src/main/java/com/brokis/Banco/controlador/dto/TransaccionDTO.java
@@ -1,9 +1,13 @@
 package com.brokis.Banco.controlador.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransaccionDTO {
 
 }
